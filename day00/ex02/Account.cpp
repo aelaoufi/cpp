9a726c94304b12ac4bd7ec132f566466d501567c
aelaoufi <aelaoufi@student.1337.ma>
2023-02-08 13:35:05 +0100

    starting day00 ex02

--- day00/ex02/Account.cpp
+++ day00/ex02/Account.cpp
@@ -6,12 +6,13 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 15:35:01 by aelaoufi          #+#    #+#             */
-/*   Updated: 2023/02/07 16:04:40 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2023/02/08 13:34:31 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "account.hpp"
-//[19920104_091532] index:0;amount:42;created
+#include <iostream>
+//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
@@ -19,7 +20,8 @@ int	Account::_totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit)
 {
-	
+	std::cout << "index:" << Account::_accountIndex << ";amount:" << Account::_amount << ";created\n";
+	Account::_accountIndex++;
 }
 
 int		Account::getNbAccounts( void ) { return (_nbAccounts); }
@@ -32,5 +34,5 @@ int		Account::getNbWithdrawals( void ) { return (_totalNbWithdrawals); }
 
 void	Account::displayAccountsInfos( void )
 {
-	
+	std::cout << "accounts:" ;
 }
