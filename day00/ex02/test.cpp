66176cf5368622c4a80c7ecbfc58810e8317c0d6
aelaoufi <aelaoufi@student.1337.ma>
2023-02-10 21:51:25 +0100

    finishing day00 ex02

--- day00/ex02/test.cpp
+++ day00/ex02/test.cpp
@@ -6,7 +6,7 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:42:16 by aelaoufi          #+#    #+#             */
-/*   Updated: 2023/02/09 15:45:39 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2023/02/10 21:51:09 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@ int main()
 	std::time_t time = std::time(0);
 	struct tm *loctime = localtime(&time);
 	
-	std::cout << loctime->tm_mday << "\n";
+	std::cout << "[" << loctime->tm_year + 1900;
+	std::cout << "0";
+	std::cout << loctime->tm_mon + 1;
 }
