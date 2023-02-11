11daa5cb045941a176c01872af1638ecd46daeb1
aelaoufi <aelaoufi@student.1337.ma>
2023-02-11 14:39:48 +0100

    finished day00 ex02

--- day00/ex02/Account.cpp
+++ day00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 15:35:01 by aelaoufi          #+#    #+#             */
-/*   Updated: 2023/02/09 15:41:15 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2023/02/11 14:32:45 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ static int _index = 0;
 
 Account::Account(int initial_deposit)
 {
+	_displayTimestamp();
 	_amount = initial_deposit;
 	_accountIndex = _index;
 	_nbDeposits = 0;
@@ -41,18 +42,21 @@ int		Account::getNbWithdrawals( void ) { return (_totalNbWithdrawals); }
 
 void	Account::displayAccountsInfos( void )
 {
+	_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() + _index << ";total:" << getTotalAmount() << ";deposits:"
 		<< getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << "\n";
 }
 
 void	Account::displayStatus (void) const
 {
+	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:"
 		<< _nbDeposits << ";withdrawals:" << _nbWithdrawals << "\n";
 }
 
 void	Account::makeDeposit(int deposit)
 {
+	_displayTimestamp();
 	_nbDeposits++;
 	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";deposit:"
 		<< deposit << ";amount:" << _amount + deposit << ";nb_deposits:" << _nbDeposits << "\n";
@@ -63,12 +67,14 @@ void	Account::makeDeposit(int deposit)
 
 int		Account::checkAmount( void ) const
 {
+	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:refused" << "\n";
 	return (0);
 }
 
 bool	Account::makeWithdrawal(int whithdrawl)
 {
+	_displayTimestamp();
 	if (_amount < whithdrawl)
 		return (Account::checkAmount());
 	_nbWithdrawals++;
@@ -82,6 +88,7 @@ bool	Account::makeWithdrawal(int whithdrawl)
 
 Account::~Account ()
 {
+	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed\n";
 }
 
@@ -90,5 +97,20 @@ void	Account::_displayTimestamp( void )
 	std::time_t time = std::time(0);
 	struct tm *loctime = localtime(&time);
 	
-	std::cout << loctime->tm_year;
+	std::cout << "[" << loctime->tm_year + 1900;
+	if (loctime->tm_mon + 1 < 10)
+		std::cout << "0";
+	std::cout << loctime->tm_mon + 1;
+	if (loctime->tm_mday < 10)
+		std::cout << "0";
+	std::cout << loctime->tm_mday << "_";
+	if (loctime->tm_hour < 10)
+		std::cout << "0";
+	std::cout << loctime->tm_hour;
+	if (loctime->tm_min < 10)
+		std::cout << "0";
+	std::cout << loctime->tm_min;
+		if (loctime->tm_sec < 10)
+		std::cout << "0";
+	std::cout << loctime->tm_sec << "] ";
 }
--- day00/ex02/test.cpp
+++ day00/ex02/test.cpp
@@ -6,7 +6,7 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:42:16 by aelaoufi          #+#    #+#             */
-/*   Updated: 2023/02/10 21:51:09 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2023/02/11 14:29:21 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,19 @@ int main()
 	struct tm *loctime = localtime(&time);
 	
 	std::cout << "[" << loctime->tm_year + 1900;
-	std::cout << "0";
+	if (loctime->tm_mon + 1 < 9)
+		std::cout << "0";
 	std::cout << loctime->tm_mon + 1;
+	if (loctime->tm_mday < 9)
+		std::cout << "0";
+	std::cout << loctime->tm_mday << "_";
+	if (loctime->tm_hour < 9)
+		std::cout << "0";
+	std::cout << loctime->tm_hour;
+	if (loctime->tm_min < 9)
+		std::cout << "0";
+	std::cout << loctime->tm_min;
+		if (loctime->tm_sec < 9)
+		std::cout << "0";
+	std::cout << loctime->tm_sec << "] ";
 }
