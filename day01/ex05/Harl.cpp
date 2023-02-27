bacf76d5da32950afb36c336b038948e03131d5b
aelaoufi <aelaoufi@student.1337.ma>
2023-02-27 13:07:46 +0100

    pull

--- day01/ex05/Harl.cpp
+++ day01/ex05/Harl.cpp
@@ -6,9 +6,23 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:17:06 by aelaoufi          #+#    #+#             */
-/*   Updated: 2023/02/24 14:17:20 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2023/02/24 15:58:10 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+void	Harl::debug()
+{ 
+	std::cout << "I love having extra bacon for my7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n"
+}
+
+void    Harl::complain(std::string level)
+{
+	void(Harl::*ft)()[4] = {debug, info, warning, error};
+}
--- day01/ex05/Harl.hpp
+++ day01/ex05/Harl.hpp
@@ -12,7 +12,7 @@ class Harl
     void    error(void);
 
     public :
-        void    complain(std::string level);    
+        void    complain(std::string level);
 };
 
 #endif
