3dc24e1806a7152dd1f5f8da7118b337d80f5980
aelaoufi <aelaoufi@student.1337.ma>
2023-03-01 13:00:12 +0100

    starting day02, ex00 is done

---
+++ day02/ex01/Fixed.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 18:17:46 by aelaoufi          #+#    #+#             */
+/*   Updated: 2023/02/28 18:18:03 by aelaoufi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	RawBits = 0;
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &_fixed) 
+{
+	std::cout << "Copy constructor called\n"; 
+	*this = _fixed;
+}
+
+Fixed &Fixed::operator= (const Fixed &_fixed)
+{
+	std::cout << "Copy assignment operator called\n";
+	RawBits = _fixed.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called\n";
+	return (RawBits);
+}
+
+void Fixed::setRawBits(int const raw) { RawBits = raw; }
+
+Fixed::~Fixed() { std::cout << "Destructor called\n"; }
--- day02/ex01/Fixed.hpp
+++ day02/ex01/Fixed.hpp
@@ -2,7 +2,20 @@
 #define FIXED_HPP
 
 #include <iostream>
-#include <string>
+#include <cmath>
 
+class Fixed
+{
+		int                 RawBits;
+		static const int    Bits = 8;
+	public :
+		Fixed();
+		Fixed(const int );
+		Fixed(const Fixed &_fixed);
+		Fixed &operator= (const Fixed &_fixed);
+		~Fixed();
+		int     getRawBits(void) const;
+		void    setRawBits(int const raw);
+};
 
 #endif
