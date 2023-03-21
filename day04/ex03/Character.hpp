5f49916ca3928297e672fc0e38581216beced6c6
aelaoufi <aelaoufi@student.1337.ma>
2023-03-21 19:56:39 +0000

    day04, Starting ex03

--- day04/ex03/Character.hpp
+++ day04/ex03/Character.hpp
@@ -3,13 +3,21 @@
 
 #include <string>
 #include "ICharacter.hpp"
+#include "AMateria.hpp"
 
 class Character : public ICharacter
 {
 	protected :
 		std::string &Name;
+		AMateria 	*slots;
 
 	public :
+		Character();
+		Character(std::string _Name);
+		Character(const Character &Charact);
+		Character &operator=(const Character &Charact);
+		~Character();
+
 		
 };
 
