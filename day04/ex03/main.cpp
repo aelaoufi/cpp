7d6a9599273d0aed8b502018fb5aef0c47af57c3
aelaoufi <aelaoufi@student.1337.ma>
2023-03-24 20:14:12 +0000

    day04, ex03 almost done

--- day04/ex03/main.cpp
+++ day04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aelaoufi <aelaoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:40:16 by aelaoufi          #+#    #+#             */
-/*   Updated: 2023/03/24 18:27:04 by aelaoufi         ###   ########.fr       */
+/*   Updated: 2023/03/24 20:12:47 by aelaoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,16 @@
 int main() 
 {
 	IMateriaSource	*src = new MateriaSource();
-	Character	self("self");
 	ICharacter		*me = new Character("me");
 	AMateria		*tmp;
 	ICharacter		*bob = new Character("bob");
 
-	Character	you = self;
 	src->learnMateria(new Cure());
 	src->learnMateria(new Ice());
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
-	self.equip(tmp);
-	delete tmp;
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
-	self.equip(tmp);
-	delete tmp;
 	me->use(0, *bob);
 	me->use(1, *bob);
 	me->use(3, *bob);
@@ -42,13 +36,9 @@ int main()
 	me->unequip(0);
 	me->use(1, *bob);
 	me->use(0, *bob);
-	you.use(0, *me);
-	you.use(1, *me);
 
 	delete bob;
 	delete me;
 	delete src;
-	delete tmp;
-	system("leaks AMateria");
 	return (0);
 }
